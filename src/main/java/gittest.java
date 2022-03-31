1aa11faea32944a54290f0075ab33adcf6615135
VectoryLee <280635315@qq.com>
2022-03-31 16:34:13 +0800

    热修提交

--- src/main/java/gittest.java
+++ src/main/java/gittest.java
@@ -2,5 +2,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("helloworl");
         System.out.println("hellogit");
+        System.out.println("hellogit4");
     }
 }
